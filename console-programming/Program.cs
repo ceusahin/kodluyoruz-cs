427a1c65e98a08aa809f532997fe0008994983d9
ceusahin <hakonsahin@gmail.com>
2023-03-02 19:53:19 +0300

    first commit

---
+++ console-programming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("First name: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Welcome! " + name + " " + surname);
+
+        }
+    }
+}
